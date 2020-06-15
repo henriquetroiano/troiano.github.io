b633bab283f6e815c96b08629c07c01bd1c8dbd6
henriquetroiano <henriquetroiano@hotmail.com>
2020-06-15 00:22:42 -0300

    new portifolio

---
+++ scripts/home.js
@@ -0,0 +1,4 @@
+document.getElementById('more').addEventListener('click', function(event) {
+  const animatedMenu = document.querySelector('.expanded')  
+  animatedMenu.classList.toggle('none')
+})
